2fa8a4e4255e5f620f7e10380009f1d417f7b102
sota <hotphilly1776@yahoo.co.jp>
2023-12-10 13:42:54 +0900

    Steering: steer nothing with trigger Fixes #7

--- toyota_steering.py
+++ toyota_steering.py
@@ -1,10 +1,20 @@
 import Adafruit_PCA9685
 import time
 
-CHANNEL_STEER = 14
-PULSE_STRAIGHT = 1
-PULSE_LEFT = 1
-PULSE_RIGHT = 1
+# ラズパイから見たPCA9685の所在地の設定
+pwm = Adafruit_PCA9685.PCA9685(address=0x40, busnum=1)
+# 通信の周波数の設定。どこからどこへの？60がいいの？
+pwm.set_pwm_freq(60)
+
+# PCA9685とモーターの接続チャネル番号
+CHANNEL_STEER = 0
+# 元に戻す、のようなものがあるのかわからなかった。荷重してからが本番か。
+PULSE_STRAIGHT = 390
+# 390, 391を境にモーター回転が反転した。
+PULSE_LEFT = 390
+PULSE_RIGHT = 391
+# テスト用の稼働時間設定
+sleeping = 1
 
 def steer_straight(pulse, sleep_time):
     print('steer_straight')
@@ -22,13 +32,13 @@ def steer_right(pulse, sleep_time):
     time.sleep(sleep_time)
 
 def	main():
-    steer_straight(PULSE_STRAIGHT, 0)
-    steer_left(PULSE_LEFT, 0)
-    steer_straight(PULSE_STRAIGHT, 0)
-    steer_right(PULSE_RIGHT, 0)
-    steer_left(PULSE_LEFT, 0)
-    steer_right(PULSE_RIGHT, 0)
-    steer_straight(PULSE_STRAIGHT, 0)
+    steer_straight(PULSE_STRAIGHT, sleeping)
+    steer_left(PULSE_LEFT, sleeping)
+    steer_straight(PULSE_STRAIGHT, sleeping)
+    steer_right(PULSE_RIGHT, sleeping)
+    steer_left(PULSE_LEFT, sleeping)
+    steer_right(PULSE_RIGHT, sleeping)
+    steer_straight(PULSE_STRAIGHT, sleeping)
 
 if __name__ == "__main__":
     main()
