368ce03199eed7e950822935b7111595fbba9166
sotanak <127929440+jizots@users.noreply.github.com>
2023-12-20 17:45:25 +0900

    Jizots/issue14
    * Shared_resource management
    Fixes #14
    
    * Shared_resource management
    Fixes #14

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-## 3つの機能をインポート
+## 自作する3つの機能をインポート
 import toyota_accel as ac
 import toyota_sensor as se
 import toyota_steering as st
-## マルチプロセスの作成
+## マルチプロセスのインポート
 from concurrent.futures import ProcessPoolExecutor ##multiprocessingよりも現代的な機能らしいので採用
-## 追加するかも要素　共有リソース管理
+## 共有リソース管理
+from multiprocessing import Manager
 
 def main():
     print('main process is starting')
-    ## 追加するかも要素　初期化、共有リソース
+    ## 共有リソースの変数の作成
+    with Manager() as manager:
+        shared_data = manager.dict()
 
 
     ## 3つの機能を別プロセスで実行する
     with ProcessPoolExecutor() as pexec:
-        pexec.submit(ac.accel)
-        pexec.submit(se.sensor)
-        pexec.submit(st.steering)
+        pexec.submit(ac.accel, shared_data)
+        pexec.submit(se.sensor, shared_data)
+        pexec.submit(st.steering, shared_data)
 
 if __name__ == "__main__":
     main()
