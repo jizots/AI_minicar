f714eb165d353fc674351ea028f74702c391c242
sotanak <127929440+jizots@users.noreply.github.com>
2024-01-17 07:13:24 +0900

    最初の分岐に吸い込まれなければ1周できる：Jizots/issue36 (#38)
    * draft
    
    * draft
    
    * draft
    
    * 1週したイエイ
    
    * 最初の分岐に吸い込まれなければ1周できるプログラム

--- toyota_accel.py
+++ toyota_accel.py
@@ -10,8 +10,8 @@ pwm.set_pwm_freq(60)
 # moving forward ~371
 # moving backward 390~
 # stop 372~389
-pwm_forward = 322
-pwm_back = 430
+pwm_forward = 319
+pwm_back = 440
 pwm_stop = 380
 
 # 前進、後進の1回の継続時間
@@ -41,10 +41,12 @@ def stop():
     pwm.set_pwm(CHANNEL_ACCEL, 0, pwm_stop)
 
 def setting(copy_data):
-    if (copy_data[1] >= 15):
+    if (copy_data[1] < 10):
+        stop()
+    elif (copy_data[1] >= 25):
         moving_forward(exec_time, pwm_forward)
     else:
-        stop()
+        moving_backward(1.0, pwm_back)
 
 def accel(shared_data):
     time.sleep(2)  # センサープロセスが先に開始するのを待つ
--- toyota_steering.py
+++ toyota_steering.py
@@ -9,14 +9,13 @@ pwm.set_pwm_freq(60)
 
 # PCA9685とモーターの接続チャネル番号
 CHANNEL_STEER = 0
-# 元に戻す、のようなものがあるのかわからなかった。荷重してからが本番か。
 # 390, 391を境にモーター回転が反転した。
 # Leftへは最低でも440はないと効かないかも.530以上は試していない。
 # Rightへは最低でも310はないと効かないかも.220以下は試していない。
 PULSE_STRAIGHT = 390
 PULSE_LEFT = 490
-PULSE_LEFT_WEEKLY = 460
-PULSE_RIGHT = 290
+PULSE_LEFT_WEEKLY = 450
+PULSE_RIGHT = 285
 PULSE_RIGHT_WEEKLY = 320
 
 # shared_dataのインデントとセンサーの関係性をわかりやすくする用
@@ -33,62 +32,56 @@ def steer_straight(pulse, sleep_time):
     time.sleep(sleep_time)
 
 def steer_left(pulse, sleep_time):
-    # print('steer_left')
+    print('steer_left')
     pwm.set_pwm(CHANNEL_STEER, 0, pulse)
     time.sleep(sleep_time)
 
 def steer_right(pulse, sleep_time):
-    # print('steer_right')
+    print('steer_right')
     pwm.set_pwm(CHANNEL_STEER, 0, pulse)
     time.sleep(sleep_time)
 
 def setting(copy_data):
-    if (copy_data[SensorIndex.F.value] < 25): #前に近い時の緊急の回避行動
-        if (copy_data[SensorIndex.FR.value] > copy_data[SensorIndex.FL.value]):
-            steer_right(PULSE_RIGHT, 0.1)
-            print("                                                  F!! turn R")
-        else:
+    if (copy_data[SensorIndex.F.value] < 65): # 前が近すぎる場合の回避
+        if (copy_data[SensorIndex.FR.value] < copy_data[SensorIndex.FL.value]): # 左がひらけている時
             steer_left(PULSE_LEFT, 0.1)
-            print("                                                  F!! turn L")
-    elif (((copy_data[SensorIndex.FL.value] < copy_data[SensorIndex.L.value]) and
-            copy_data[SensorIndex.L.value] < 30) or 
-            (copy_data[SensorIndex.FL.value] < 15)): #壁に近く and 車体が左に傾いている <- 傾いている時の精度は当てに全くならないが大小なら比較できる
-        steer_right(PULSE_RIGHT, 0.1)  #FLが近い時には強制的に曲がる
-        steer_straight(PULSE_STRAIGHT, 0.02)
-        print("                                                  R")
-    elif (((copy_data[SensorIndex.FR.value] < copy_data[SensorIndex.R.value]) and
-            copy_data[SensorIndex.R.value] < 30) or 
-            (copy_data[SensorIndex.FR.value] < 15)): #壁に近く and 車体が右に傾いている
-        steer_left(PULSE_LEFT, 0.1)  #FRが近い時には強制的に曲がる
-        steer_straight(PULSE_STRAIGHT, 0.02)
-        print("                                                  L")
-    elif (copy_data[SensorIndex.R.value] > 35 and
-        (copy_data[SensorIndex.FR.value] > copy_data[SensorIndex.R.value])): #右壁から遠ざかりすぎているので近づくように右に曲がる
-        steer_right(PULSE_RIGHT_WEEKLY, 0.1)
-        steer_straight(PULSE_STRAIGHT, 0.02)
-        print("                                                  weak R")
-    elif (copy_data[SensorIndex.R.value] < 10 and
-        (copy_data[SensorIndex.FR.value] > copy_data[SensorIndex.R.value])): #右壁から近すぎているので近づくように左に曲がる
-        steer_left(PULSE_LEFT_WEEKLY, 0.1)
-        steer_straight(PULSE_STRAIGHT, 0.02)
-        print("                                                  weak L")
+            steer_straight(PULSE_STRAIGHT, 0.1)
+        else: # 右がひらけている時
+            steer_right(PULSE_RIGHT, 0.1)
+            steer_straight(PULSE_STRAIGHT, 0.1)
+    elif (copy_data[SensorIndex.FL.value] < 40 or
+        copy_data[SensorIndex.L.value] < 30): # 左が近すぎる場合の回避
+        steer_right(PULSE_RIGHT, 0.15)
+        steer_straight(PULSE_STRAIGHT, 0.05)
+    elif (copy_data[SensorIndex.FR.value] < 40 or
+        copy_data[SensorIndex.R.value] < 30):# 右が近すぎる場合の回避
+        steer_left(PULSE_LEFT, 0.15)
+        steer_straight(PULSE_STRAIGHT, 0.05)
+    elif (copy_data[SensorIndex.F.value > 80]):
+        steer_straight(PULSE_STRAIGHT, 0.1)
+    elif (copy_data[SensorIndex.F.value] <
+          (copy_data[SensorIndex.FR.value])): # 右前がひらけている時 Try2の場合は以下コメントアウト
+        steer_right(PULSE_RIGHT_WEEKLY, 0.15)
+        steer_straight(PULSE_STRAIGHT, 0.1)
+    elif (copy_data[SensorIndex.F.value] <
+          (copy_data[SensorIndex.FL.value])): # 左前がひらけている時
+        steer_left(PULSE_LEFT_WEEKLY, 0.15)
+        steer_straight(PULSE_STRAIGHT, 0.1)
     else:
         steer_straight(PULSE_STRAIGHT, 0.1)
-        print("                                                  GO")
 
 def steering(shared_data):
     time.sleep(2) # センサープロセスが先に開始するのを待つ
     copy_data = [0,0,0,0,0]
     while True:
-        copy_data[SensorIndex.FL.value] = shared_data[0]  # front_left
-        copy_data[SensorIndex.F.value] = shared_data[1]  # front
-        copy_data[SensorIndex.FR.value] = shared_data[2]  # front_right
-        copy_data[SensorIndex.L.value] = shared_data[3]  # left
-        copy_data[SensorIndex.R.value] = shared_data[4]  # right
+        copy_data[SensorIndex.FL.value] = shared_data[0]
+        copy_data[SensorIndex.F.value] = shared_data[1]
+        copy_data[SensorIndex.FR.value] = shared_data[2]
+        copy_data[SensorIndex.L.value] = shared_data[3]
+        copy_data[SensorIndex.R.value] = shared_data[4]
         print(f"Steering: L:{copy_data[SensorIndex.L.value]}, \
 FL:{copy_data[SensorIndex.FL.value]}, \
 F:{copy_data[SensorIndex.F.value]}, \
 FR:{copy_data[SensorIndex.FR.value]}, \
 R:{copy_data[SensorIndex.R.value]}")
-
         setting(copy_data)
